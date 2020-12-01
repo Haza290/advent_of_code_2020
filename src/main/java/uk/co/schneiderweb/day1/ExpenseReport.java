32d59648c401399cf1d3cdf4d46759c14b2340fb
Harry <Harry.Schneider@AquilaHeywood.co.uk>
2020-12-01 14:32:05 +0000

    Solution for day 1 part 2

--- src/main/java/uk/co/schneiderweb/day1/ExpenseReport.java
+++ src/main/java/uk/co/schneiderweb/day1/ExpenseReport.java
@@ -2,18 +2,34 @@ package uk.co.schneiderweb.day1;
 
 import uk.co.util.ReadFile;
 
-import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 
 public class ExpenseReport {
 
     public static void main(String[] args) {
         List<Integer> inputArray = ReadFile.readIntegerListFromFile(ExpenseReport.class.getClassLoader().getResource("day1_input"));
-        System.out.println(new ExpenseReport().solve(inputArray));
+        System.out.println("Solution for 2 elements: " + new ExpenseReport().solve(inputArray, 2));
+        System.out.println("Solution for 3 elements: " + new ExpenseReport().solve(inputArray, 3));
     }
 
-    public Integer solve(List<Integer> inputArray) {
-        Integer first = inputArray.stream().filter(i -> inputArray.contains(2020 - i)).findAny().get();
-        return first * (2020 - first);
+    Integer solve(List<Integer> inputArray, int elements) {
+        if (elements == 2) {
+            int[] result = getPairsSummingTarget(inputArray, 2020);
+            return result[0] * result[1];
+        }
+
+        Integer first = inputArray.stream().filter(i -> getPairsSummingTarget(inputArray, 2020-i) != null).findAny().get();
+        int[] result = getPairsSummingTarget(inputArray, 2020 - first);
+        return first * result[0] * result[1];
+    }
+
+    int[] getPairsSummingTarget(List<Integer> inputArray, int target) {
+        Optional<Integer> first = inputArray.stream().filter(i -> inputArray.contains(target - i)).findAny();
+        if (first.isPresent()) {
+            int[] result = {first.get(), target - first.get()};
+            return result;
+        }
+        return null;
     }
 }
--- src/test/java/uk/co/schneiderweb/day1/ExpenseReportTest.java
+++ src/test/java/uk/co/schneiderweb/day1/ExpenseReportTest.java
@@ -12,12 +12,28 @@ import static org.junit.Assert.assertThat;
 public class ExpenseReportTest {
 
     @Test
-    public void expenseReportTest() {
+    public void expenseReportPart1Test() {
         ExpenseReport expenseReport = new ExpenseReport();
         List<Integer> inputArray1 = Arrays.asList(1721, 979, 366, 299, 675, 1456);
         List<Integer> inputArray2 = Arrays.asList(23, 1721, 979, 366, 299, 675, 1456);
-        assertThat(expenseReport.solve(inputArray1), is(equalTo(514579)));
-        assertThat(expenseReport.solve(inputArray2), is(equalTo(514579)));
+        assertThat(expenseReport.solve(inputArray1, 2), is(equalTo(514579)));
+        assertThat(expenseReport.solve(inputArray2, 2), is(equalTo(514579)));
+    }
+
+    @Test
+    public void expenseReportPart2Test() {
+
+        ExpenseReport expenseReport = new ExpenseReport();
+        List<Integer> inputArray1 = Arrays.asList(1721, 979, 366, 299, 675, 1456);
+        assertThat(expenseReport.solve(inputArray1, 3), is(equalTo(241861950)));
+    }
+
+    @Test
+    public void getPairsSummingTargetTest() {
+        ExpenseReport expenseReport = new ExpenseReport();
+        List<Integer> inputArray1 = Arrays.asList(1721, 979, 366, 299, 675, 1456);
+        int[] expected = {1721, 299};
+        assertThat(expenseReport.getPairsSummingTarget(inputArray1, 2020), is(equalTo(expected)));
     }
 
 }
