47d60375975c9dc38b0e47c9997270cbff7cc8b0
Harry <Harry.Schneider@AquilaHeywood.co.uk>
2020-12-08 08:42:30 +0000

    Solution to day 7 part 2
    Still needs some refactoring when I get a chance

--- src/main/java/uk/co/schneiderweb/day7/HandyHaversacks.java
+++ src/main/java/uk/co/schneiderweb/day7/HandyHaversacks.java
@@ -6,8 +6,10 @@ import uk.co.schneiderweb.util.ReadFile;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -18,7 +20,7 @@ public class HandyHaversacks {
     public static void main(String[] args) {
         List<String> input = ReadFile.readStringListFromFile(HandyHaversacks.class.getClassLoader().getResource("day7_input"));
         HandyHaversacks handyHaversacks = new HandyHaversacks();
-        System.out.println(handyHaversacks.numberOfBagsThatCanContainGold(input));
+        System.out.println(handyHaversacks.numberOfBagsThatGoldContains(input));
     }
 
     public int numberOfBagsThatCanContainGold(List<String> inputList) {
@@ -31,16 +33,33 @@ public class HandyHaversacks {
         return bagsThatContainGold.size() - 1;
     }
 
+    public int numberOfBagsThatGoldContains(List<String> inputList) {
+        HashMap<String, Integer> bagsContentsHashMap = new HashMap<>();
+        List<Bag> bags = inputList.stream().map(this::stringToBag).collect(Collectors.toList());
+
+        // initialise the bagsContentsHashMap with empty bags
+        bags.stream().filter(bag -> bag.getBagsThatCanBeContained().isEmpty()).forEach(bag -> bagsContentsHashMap.put(bag.getColour(),1));
+
+        while (!bagsContentsHashMap.containsKey("shiny gold")) {
+            bags.stream()
+                    .filter(bag -> !bagsContentsHashMap.containsKey(bag.getColour()))
+                    .filter(bag -> bagsContentsHashMap.keySet().containsAll(bag.getBagsThatCanBeContained().keySet()))
+                    .forEach(bag -> bagsContentsHashMap.put(bag.getColour(), bag.getNumberOfBagsInside(bagsContentsHashMap)));
+        }
+
+
+        return bagsContentsHashMap.get("shiny gold") - 1;
+    }
+
     private Bag stringToBag(String bagString) {
         String[] bagStringSplit = bagString.split(" bags contain ");
         String colour = bagStringSplit[0];
-        Set<String> bagsThatCanBeContained;
-        if (bagStringSplit[1] == "no other bags.") {
-            bagsThatCanBeContained = new HashSet<>();
+        HashMap<String, Integer> bagsThatCanBeContained;
+        if (bagStringSplit[1].equals("no other bags.")) {
+            bagsThatCanBeContained = new HashMap<>();
         } else {
-            bagsThatCanBeContained = Arrays.stream(bagStringSplit[1].split(", "))
-                    .map(s -> s.substring(s.indexOf(' ') + 1, s.lastIndexOf(' ')))
-                    .collect(Collectors.toSet());
+            bagsThatCanBeContained = (HashMap<String, Integer>)Arrays.stream(bagStringSplit[1].split(", "))
+                    .collect(Collectors.toMap(s -> s.substring(s.indexOf(' ') + 1, s.lastIndexOf(' ')), s -> Integer.valueOf(s.substring(0,s.indexOf(' ')))));
         }
         return new Bag(colour, bagsThatCanBeContained);
     }
@@ -48,10 +67,14 @@ public class HandyHaversacks {
     @Data
     public class Bag {
         private final String colour;
-        private final Set<String> bagsThatCanBeContained;
+        private final HashMap<String, Integer> bagsThatCanBeContained;
 
         public boolean containsBags(Set<String> bags) {
-            return bagsThatCanBeContained.stream().anyMatch(bags::contains);
+            return true;//bagsThatCanBeContained.stream().anyMatch(bags::contains);
+        }
+
+        public int getNumberOfBagsInside(HashMap<String, Integer> hashMap) {
+            return bagsThatCanBeContained.keySet().stream().reduce(0, (subtotal, colour) -> subtotal + bagsThatCanBeContained.get(colour) * hashMap.get(colour), Integer::sum) + 1;
         }
     }
 }
